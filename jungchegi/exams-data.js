d97f318ca125248f96396a4c31fca9dc5fef7cc9
soondooop <52400032+soondooop@users.noreply.github.com>
2026-09-11 17:15:03 +0900

    Add 정체기 quiz page at /jungchegi without changing the portfolio homepage.

---
+++ jungchegi/exams-data.js
@@ -0,0 +1,9 @@
+const EXAMS = Object.assign({},
+  (typeof EXAMS_2020 === "undefined" ? {} : EXAMS_2020),
+  (typeof EXAMS_2021 === "undefined" ? {} : EXAMS_2021),
+  (typeof EXAMS_2022 === "undefined" ? {} : EXAMS_2022),
+  (typeof EXAMS_2023 === "undefined" ? {} : EXAMS_2023),
+  (typeof EXAMS_2024 === "undefined" ? {} : EXAMS_2024),
+  (typeof EXAMS_2025 === "undefined" ? {} : EXAMS_2025),
+  (typeof EXAMS_2026 === "undefined" ? {} : EXAMS_2026)
+);
